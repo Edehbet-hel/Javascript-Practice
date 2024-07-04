ac90e6d8f4f8cc684e829e1ae60cee0f2d42804a
Bethel <bethelededh668@gmail.com>
2024-07-04 09:37:18 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+
+function addnum(number){
+    let sum=0
+    for( let i=0; i<= number; i=i+2){
+          if (number%2!==0) {
+            console.log("not an even number");
+          }else{
+            sum = sum +i
+          }
+
+    }
+    return sum
+}
+let grades=[["sam", "john","jones"],83,["kelvin","hart",],85,["durant","kevin"],93,72]
+console.log( `${grades[0][2]} is not ${grades[0][0]} but ${grades[4][1]} ${grades[0][1]} is ${grades[3]} years old`)
+//jones is not sam but kevin john is 85 years old
+grades.pop()
+ let removed =grades.pop()
+ grades,push("i am the new girl in town")
+
+ console.log(grades);
+
+ console.log(`i was $(removed)`)
